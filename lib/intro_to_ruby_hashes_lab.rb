04d5b9cd3837abb8715dd7411ab068cea6e507be
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-10 02:36:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ end
   # if the provided key is present, increment its value by 1
   
 def update_counting_hash(hash, key)
-  if hash[key] 
+  if hash[key]
     hash[key] += 1
   else 
     hash[key] = 1
