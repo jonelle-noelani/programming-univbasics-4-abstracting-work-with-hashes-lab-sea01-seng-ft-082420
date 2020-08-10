7dfc6a9bdcd753bcc7013bef89a69fdb4827875f
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-10 02:29:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,10 +4,10 @@ def my_hash_creator(key, value)
   #{:key => value}
 end
 
-# def read_from_hash(hash, key)
-#   # return the correct value using the hash and key parameters
-#   hash[key]
-# end
+## return the correct value using the hash and key parameters 
+def read_from_hash(hash, key)
+  hash[key]
+end
 
 # def update_counting_hash(hash, key)
 #   # given a hash an a key as parameters, return an updated hash
