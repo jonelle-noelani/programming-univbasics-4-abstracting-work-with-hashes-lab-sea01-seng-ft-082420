a225e31a2816833ffbced853a1b74afacbb3e351
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-10 02:31:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
   # return a hash that includes the key and value parameters passed into this method
-def my_hash_creator(key, value)
-  {key: value}
-  #{:key => value}
-end
+# def my_hash_creator(key, value)
+#   {key: value}
+#   #{:key => value}
+# end
 
 ## return the correct value using the hash and key parameters 
 def read_from_hash(hash, key)
